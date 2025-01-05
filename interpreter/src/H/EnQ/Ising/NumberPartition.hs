10a8f6271f4bed72c483f49983ff0410294d8a52
James Bryan Graves <jbg@Macintosh.local>
2025-01-05 18:46:17 +0100

    fix for Ising Cliques

--- interpreter/src/H/EnQ/Ising/NumberPartition.hs
+++ interpreter/src/H/EnQ/Ising/NumberPartition.hs
@@ -1,9 +1,18 @@
 module H.EnQ.Ising.NumberPartition
     ( numberPartitionH
-    , testNumberPartitionH
+    , solveNumberPartition
     ) where
 
+import Control.Monad (replicateM)
+import System.Random (randomRIO)
+
 import H.EnQ.Ising.Ising (generateSpins, solveHamiltonians, findMinimum, solveClassical, solveQuantum, suggestT)
+
+-- Generate a random collection of numbers (1 to 9) of length 2 to 9
+randomNumbers :: IO [Int]
+randomNumbers = do
+    len <- randomRIO (2, 9)               -- Random length between 2 and 9
+    replicateM len (randomRIO (1, 9))    -- Generate random numbers in the range [1, 9]
 
 -- Define the Hamiltonian for the number partitioning problem
 numberPartitionH :: [Int] -> [Int] -> Double
@@ -11,9 +20,12 @@ numberPartitionH numbers spins =
   let weightedSum = sum $ zipWith (*) numbers spins
   in fromIntegral (weightedSum ^ 2)
 
-testNumberPartitionH :: IO ()
-testNumberPartitionH = do
-  let numbers = [3, 1, 4, 2, 2]
+solveNumberPartition :: IO ()
+solveNumberPartition = do
+  -- let numbers = [3, 1, 4, 2, 2]
+  numbers <- randomNumbers
+  putStrLn $ "Random numbers to partition: " ++ show numbers
+
   let numSpins = length numbers
   let hamiltonian spins = numberPartitionH numbers spins
   let shots = 1024
